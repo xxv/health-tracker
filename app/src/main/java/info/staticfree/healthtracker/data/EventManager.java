ed89af302c098cd7b719fd171dcf033c2c514db4
Steve Pomeroy <steve@staticfree.info>
2015-12-27 13:55:14 -0500

    Adds callbacks to InsertOrUpdate

--- app/src/main/java/info/staticfree/healthtracker/data/EventManager.java
+++ app/src/main/java/info/staticfree/healthtracker/data/EventManager.java
@@ -5,17 +5,35 @@ import android.content.ContentUris;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import java.lang.ref.WeakReference;
 
 public class EventManager {
     /**
      * Inserts or updates, based on when the last entry was made.
      */
-    public void insertOrUpdate(final ContentResolver resolver, final Uri uri,
-            final ContentValues values, final long when, final long deDuplicateWindow) {
+    public void insertOrUpdate(@NonNull final InsertOrUpdateListener listener,
+            final ContentResolver resolver, final Uri uri, final ContentValues values,
+            final long when, final long deDuplicateWindow) {
+        final InsertOrUpdateHandler handler =
+                new InsertOrUpdateHandler(Looper.getMainLooper(), listener);
+        final WeakReference<InsertOrUpdateHandler> handlerRef = new WeakReference<>(handler);
         final Runnable worker = new Runnable() {
             @Override
             public void run() {
-                insertOrUpdateInternal(resolver, uri, values, when, deDuplicateWindow);
+                final InsertOrUpdateResult result =
+                        insertOrUpdateInternal(resolver, uri, values, when, deDuplicateWindow);
+                final InsertOrUpdateHandler handlerInternal = handlerRef.get();
+
+                if (handlerInternal != null) {
+                    handlerInternal.obtainMessage(InsertOrUpdateHandler.MSG_RESULT, result)
+                            .sendToTarget();
+                }
             }
         };
 
@@ -25,9 +43,11 @@ public class EventManager {
     /**
      * Inserts or updates, based on when the last entry was made.
      */
-    private Uri insertOrUpdateInternal(final ContentResolver resolver, final Uri uri,
-            final ContentValues values, final long when, final long deDuplicateWindow) {
+    private InsertOrUpdateResult insertOrUpdateInternal(final ContentResolver resolver,
+            final Uri uri, final ContentValues values, final long when,
+            final long deDuplicateWindow) {
         final Uri content;
+        final boolean isInsert;
 
         final Cursor inWindow = resolver.query(uri, new String[] { Event.EVENT_DATE, Event._ID },
                 Event.EVENT_DATE + " >= (? - ?)", new String[] {
@@ -44,8 +64,10 @@ public class EventManager {
                             inWindow.getLong(inWindow.getColumnIndexOrThrow(Event._ID));
                     content = ContentUris.withAppendedId(uri, existingId);
                     resolver.update(content, values, null, null);
+                    isInsert = false;
                 } else {
                     content = resolver.insert(uri, values);
+                    isInsert = true;
                 }
             } finally {
                 inWindow.close();
@@ -54,6 +76,43 @@ public class EventManager {
             throw new IllegalStateException("Received null cursor");
         }
 
-        return content;
+        return new InsertOrUpdateResult(content, isInsert);
+    }
+
+    public interface InsertOrUpdateListener {
+        void onInsertOrUpdate(@NonNull final InsertOrUpdateResult insertOrUpdateResult);
+    }
+
+    public static final class InsertOrUpdateResult {
+        public final boolean mIsInsert;
+        @Nullable
+        public final Uri mUri;
+
+        InsertOrUpdateResult(@Nullable final Uri uri, final boolean isInsert) {
+            mUri = uri;
+            mIsInsert = isInsert;
+        }
+    }
+
+    public static class InsertOrUpdateHandler extends Handler {
+        public static final int MSG_RESULT = 100;
+        private final WeakReference<InsertOrUpdateListener> mListenerRef;
+
+        public InsertOrUpdateHandler(@NonNull final Looper looper,
+                @NonNull final InsertOrUpdateListener listener) {
+            super(looper);
+            mListenerRef = new WeakReference<>(listener);
+        }
+
+        @Override
+        public void handleMessage(final Message msg) {
+            if (MSG_RESULT == msg.what) {
+                final InsertOrUpdateListener listener = mListenerRef.get();
+
+                if (listener != null) {
+                    listener.onInsertOrUpdate((InsertOrUpdateResult) msg.obj);
+                }
+            }
+        }
     }
 }
--- app/src/main/java/info/staticfree/healthtracker/ui/PainEntryFragment.java
+++ app/src/main/java/info/staticfree/healthtracker/ui/PainEntryFragment.java
@@ -2,30 +2,56 @@ package info.staticfree.healthtracker.ui;
 
 import android.content.ContentValues;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RatingBar;
+import android.widget.TextView;
 
 import java.util.concurrent.TimeUnit;
 
 import info.staticfree.healthtracker.R;
+import info.staticfree.healthtracker.data.EventManager;
+import info.staticfree.healthtracker.data.EventManager.InsertOrUpdateListener;
 import info.staticfree.healthtracker.data.MeasurementEvent;
 
 public class PainEntryFragment extends Fragment {
+    private TextView mPainEntryValue;
     private final RatingBar.OnRatingBarChangeListener mRatingChange =
             new RatingBar.OnRatingBarChangeListener() {
+
+                private final InsertOrUpdateListener mListener = new InsertOrUpdateListener() {
+
+                    @Override
+                    public void onInsertOrUpdate(
+                            @NonNull final EventManager.InsertOrUpdateResult insertOrUpdateResult) {
+
+                        if (insertOrUpdateResult.mIsInsert) {
+                            Snackbar.make(getView(), R.string.notice_new_entry_created,
+                                    Snackbar.LENGTH_LONG).show();
+                        } else {
+
+                            Snackbar.make(getView(), R.string.notice_last_entry_updated,
+                                    Snackbar.LENGTH_LONG).show();
+                        }
+                    }
+                };
 
                 @Override
                 public void onRatingChanged(final RatingBar ratingBar, final float rating,
                         final boolean fromUser) {
+                    mPainEntryValue.setText(String.format("%.1f", rating));
+
                     if (fromUser) {
                         final ContentValues values = new ContentValues();
                         values.put(MeasurementEvent.VALUE, String.valueOf(rating));
-                        MeasurementEvent.MANAGER.insertOrUpdate(getActivity().getContentResolver(),
-                                MeasurementEvent.CONTENT_URI, values, System.currentTimeMillis(),
-                                TimeUnit.SECONDS.toMillis(30));
+                        MeasurementEvent.MANAGER
+                                .insertOrUpdate(mListener, getActivity().getContentResolver(),
+                                        MeasurementEvent.CONTENT_URI, values,
+                                        System.currentTimeMillis(), TimeUnit.SECONDS.toMillis(30));
                     }
                 }
             };
@@ -53,6 +79,7 @@ public class PainEntryFragment extends Fragment {
             final Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_pain_entry, container, false);
         ((RatingBar) view.findViewById(R.id.ratingBar)).setOnRatingBarChangeListener(mRatingChange);
+        mPainEntryValue = (TextView) view.findViewById(R.id.pain_entry_value);
 
         return view;
     }
